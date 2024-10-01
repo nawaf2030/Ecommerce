ec2412b6a044ed10d806c6666085b71546e07203
nawaf alrashidi <o0002012000o@hotmail.com>
2024-10-01 18:22:50 +0300

    test1

---
+++ Ecommerce/Ecommerce.Application/Contracts/Services/CategoryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Contracts.Services
+{
+	public class CategoryService
+	{
+	}
+}
